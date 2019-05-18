97ff1cdea226a5ae36125f1c4798ac9058e4a44a
Yasutaka <Yasutaka@192.168.10.10>
2019-05-18 19:32:13 +0900

    こんにちはGitさん

--- HelloGitHub/src/GitHubTest.java
+++ HelloGitHub/src/GitHubTest.java
@@ -4,6 +4,10 @@ public class GitHubTest {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello GitHub");
+		
+		String git="こんにちは！Git";
+		
+		System.out.println(git);
 	}
 
 }
